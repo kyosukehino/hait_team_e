9394d3c34aca67ebb51e6ba98987c3778a40e1cb
Toshiki-Nishimura <58242483+Toshiki-Nishimura@users.noreply.github.com>
2019-12-15 15:59:51 +0900

    Update views.py

--- housepriceproject.py/housepriceproject/housepriceproject/views.py
+++ housepriceproject.py/housepriceproject/housepriceproject/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 def mainfunc(request):
     if request.method == 'POST[inlineRadioOptions6]':
-        hiyoshi = request.POST['inlineRadioOptions6']
+    return render(request, 'main.html', {})
+    
+    elif request.method == 'POST[inlineRadioOptions5]':
+    return render(request, 'main.html', {})
 
 
     return render(request, 'main.html', {})
