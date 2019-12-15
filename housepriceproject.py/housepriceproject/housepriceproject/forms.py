829ebb74fe79f8f30b1b4e6b35b792ac42c3e55f
Toshiki-Nishimura <58242483+Toshiki-Nishimura@users.noreply.github.com>
2019-12-15 19:24:26 +0900

    Update forms.py

--- housepriceproject.py/housepriceproject/housepriceproject/forms.py
+++ housepriceproject.py/housepriceproject/housepriceproject/forms.py
@@ -1,1 +1,8 @@
+from django.forms import ModelForm
+from .models　import House
+#.modelsはmodel.pyをさす
 
+Class House(ModelForm):
+    class Meta:
+      model=House
+      fields=['house_station','house_walk','house_year']
