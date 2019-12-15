b66469e9de90a62b1a62f86257a1ec5238bce6a0
Toshiki-Nishimura <58242483+Toshiki-Nishimura@users.noreply.github.com>
2019-12-15 19:24:57 +0900

    Update models.py

--- housepriceproject.py/housepriceproject/housepriceproject/models.py
+++ housepriceproject.py/housepriceproject/housepriceproject/models.py
@@ -1,1 +1,13 @@
+from django.db import models
+from django.utils import timezone
 
+class House(models.Model):#ジャンゴに元々入っているモデルの大枠らしい
+
+  
+    class Meta: #Meta情報：データベースの名前を決めるやつ
+        db_table='houseimformation'
+    
+    house_station=models.CharField('駅',max_length=10)
+    house_walk=models.IntegerField('徒歩分')
+    house_year=models.IntegerField('築年数')
+   # house_unknown=models..CharField('未定',max_length=10)
